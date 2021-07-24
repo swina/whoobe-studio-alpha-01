c6c2d38bf0547278ad18f47474243081a334fd7e
Swina Allen <swina.allen@gmail.com>
2021-07-25 02:34:20 +0300

    Added spin animation for waiting server

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="cursor-pointer bg-gray-800 bg-center bg-cover bg-no-repeat h-screen w-screen flex flex-col items-center justify-center" title="Click to start" style="background-image:url(https://res.cloudinary.com/moodgiver/image/upload/v1616863115/desktop-1155613_1920_eimtxw.jpg);">
       <div class="m-auto text-center font-thin  m-auto">
-        <img src="../assets/logo.svg" class="w-64 grayscale"/>
+        <img src="../assets/logo.svg" class="w-64 grayscale animate-spin"/>
         <div class="text-gray-700 text-sm -mt-4w-64 text-right font-hairline">S T U D I O</div>
-        <div v-if=""
+        <div v-if="!$store.state.user.login">Waiting for Whoobe Server ... please wait</div>
         <button v-if="$store.state.user.login" @click="$router.push('desktop')" class="text-2xl p-4">Start</button>
         <!-- <div class="tex-left">
           <login v-if="!logged" @islogged="check" class="m-auto w-64"/>
